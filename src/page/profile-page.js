f8e3cbea9996ae60d666142c53ee4dfa6130368a
Bekket McClane <yihshyng223@gmail.com>
2018-10-09 13:03:58 -0700

    Update profile content

--- src/page/profile-page.js
+++ src/page/profile-page.js
@@ -5,6 +5,7 @@ import '../../node_modules/@polymer/iron-icons/communication-icons.js';
 import '../../node_modules/@polymer/paper-button/paper-button.js';
 import '../../node_modules/@polymer/paper-styles/color.js';
 import '../../node_modules/@polymer/paper-dialog/paper-dialog.js';
+import '../../node_modules/@polymer/paper-icon-button/paper-icon-button.js';
 import { html } from '../../node_modules/@polymer/polymer/lib/utils/html-tag.js';
 /*
  * @customElement
@@ -72,8 +73,8 @@ class ProfilePage extends PolymerElement {
         </paper-button>
         <br><br><br>
         <span>
-          I'm currently a computer science major Ph.D. student in UCI, advised by <a href="https://www.michaelfranz.com/" target="_blank">Prof. Michael Franz</a>.&nbsp;
-          I received my B.S. degree in computer science from National Tsing Hua University, Taiwan, in 2018.
+          I'm currently a Ph.D. student major in computer science in UCI, advised by Prof. Michael Franz.&nbsp;
+          I received my Bachelor degree in computer science from National Tsing Hua University, Taiwan, in 2018.
           <br><br>
           My research interests includes: 
           <b>Compilers, High Performance Computing, Security and System Software</b>.
@@ -85,7 +86,7 @@ class ProfilePage extends PolymerElement {
         <br><br>
         <span>
           In my leisure time, I like jogging, coffee brewing, 
-          and listening <a href="https://en.wikipedia.org/wiki/Nightcore" target="_blank">Nightcore</a> music.
+          and listening Nightcore<a href="https://en.wikipedia.org/wiki/Nightcore" target="_blank"><paper-icon-button icon="launch"></paper-icon-button></a> music.
         </span>
       </p>
     </div>
