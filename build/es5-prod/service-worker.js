7a5060f30ce17a2b3730a03db87bde7ae0e6fa1f
Min-Yih Hsu <yihshyng223@gmail.com>
2022-04-21 11:58:04 -0700

    Update Resume: rebuild

--- build/es5-prod/service-worker.js
+++ build/es5-prod/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","e9b3d1538e9e4caad4b613f9961f4e33"],["src/page/experience-page.js","cbb84aeadad54d4cfc0dd4c6af3da68a"],["src/page/landing-page.js","3d2bd9c0744902c0a061e404dd704982"],["src/page/profile-page.js","f2f4788a3a6f70b0578571dcfbce1234"],["src/page/project-page.js","a3313d516d99f095a4bfefee21af6832"],["src/page/publication-page.js","0ca0cf9d00ac80653ee117e2af840714"]];
+var precacheConfig = [["index.html","e9b3d1538e9e4caad4b613f9961f4e33"],["src/page/experience-page.js","50dae8489b616d73737deaba9d5116d8"],["src/page/landing-page.js","d627b4134adb438391ae59742387f9d3"],["src/page/profile-page.js","6e037ebb23e1d58a35db3ab84ad46ac8"],["src/page/project-page.js","8f42b38919f40e245b921edcbb2bb2e5"],["src/page/publication-page.js","a10be0f61baafc8a10f90344994c0630"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
